4ccdd48732b36d00216dadb0eb95ba237f025352
zhu.jiang <zhu.jiang@56qq.com>
2019-04-24 16:33:33 +0800

    增加历史数据保存

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+const mongoose = require('mongoose');
 const server = require('http').createServer();
 const io = require('socket.io')(server);
+
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection mongodb error:'));
+db.once('open', function() {
+  // we're connected!
+});
+// events => type event
+const eventSchema = new mongoose.Schema({
+  rid: String,
+  type: String,
+  event: Object,
+});
+const Event = mongoose.model('Event', eventSchema);
 
 
 io.on('connection', function(client) {
   const query = client.handshake.query;
   const room = query.r;
-  
+  const historyEvents = Event.find({ rid: room });
+
+  console.log('客户端连接 ' + client.id);
+  historyEvents.then((result) => {
+    console.log(client.id + '查询历史事件数据 > ');
+    io.sockets.to(client.id).emit('initEvents', result);
+  })
   client.join(room);
   // 客户端离开
   client.on('disconnect', async () => {
@@ -14,19 +35,19 @@ io.on('connection', function(client) {
   client.on('event', (data) => {
     const rooms = io.sockets.adapter.rooms[room];
     const sockets = Object.keys(rooms.sockets).filter((item) => item !== client.id);
+    const saveEvent = new Event({rid: room, ...data});
 
-    console.log('socket id > ', client.id);
-    console.log('rooms > ', client.rooms);
-    // console.log('clients > ', io.sockets) // .clients(room)
-    console.log('other sockets > ', sockets)
+    saveEvent.save(function(err, e) {
+      err && console.log('err > ', err);
+    })
+    // console.log('socket id > ', client.id);
+    // console.log('other sockets > ', sockets)
     let sendTo = null;
     sockets.forEach((s) => {
       sendTo = io.to(s);
     });
     sendTo && sendTo.emit('event', data);
-    // io.to(room).emit('event', data);
   });
-  // io.to(room).emit('welcome', 'welcome to ' + room);
 });
 
 
--- client.js
+++ client.js
@@ -2,35 +2,75 @@ $(function() {
   var url = '0.0.0.0:1024';
   var qs = Qs.parse(location.search.slice(1));
   var ignoreRemoteEvent = false;
+  var events = [];
   var socket = io(url, {
     query: { r: qs.r }
   });
-  var pushEventsQueue = async.queue(function pushEventsQueue(data, callback) {
-    console.log('emit')
-    if (data.type === 'editor') {
-      socket.emit('event', { event: data.task }, function(data) {
-				callback();
-			});
-    }
-  }, 1);
 
-
+  // 这里要保留数据让编辑器加载完毕后进行初始化, 有更好的方式?
+  socket.on('initEvents', function(result) {
+    events = result;
+  });
   require(["vs/editor/editor.main"], function () {
     editor = monaco.editor.create(document.getElementById('monaco'), {
       language: 'javascript',
-      // theme: 'vs-dark',
+      theme: 'vs-dark',
     });
     editor.onDidChangeModelContent(function(change) {
       if (ignoreRemoteEvent) {
         return;
       }
-      // pushEventsQueue.push({ type: 'editor', task: change }, function() {});
-      socket.emit('event', { type: 'editor', task: change }, function(data) {
+      socket.emit('event', { type: 'editor', event: change }, function(data) {
 				callback();
 			});
     });
+    function initEvents() {
+      const targetEventIndex = events.length - 1;
+      // 0 -> end - 1
+      for(var i = 0; i <= targetEventIndex; i++) {
+        var item = events[i];
+        var event = events[i].event;
+        if (item.type === 'editor') {
+          for(var j = 0; j < event.changes.length; j++) {
+            var change = event.changes[j];
+            // Refer to: https://github.com/Microsoft/monaco-editor/issues/432.
+            try {
+              ignoreRemoteEvent = true;
+              console.log('!!! > ', change)
+              executeEdits([
+                {
+                  identifier: 'playback',
+                  range: new monaco.Range(
+                    change.range.startLineNumber,
+                    change.range.startColumn,
+                    change.range.endLineNumber,
+                    change.range.endColumn),
+                  text: change.text
+                }
+              ]);
+            } finally {
+              ignoreRemoteEvent = false;
+            }
+          }
+        }
+      }
+      ignoreRemoteEvent = false;
+    }
+    function executeEdits(edits, endCursorState) {
+      editor.getModel().pushEditOperations(editor.getSelections(), edits, function() {
+        return endCursorState ? endCursorState : null;
+      });
+      editor.getModel().pushStackElement();
+      if (endCursorState) {
+        editor.setSelections(endCursorState);
+      }
+    }
+
+    setTimeout(() => {
+      initEvents()
+    }, 1000)
     socket.on('event', function(data) {
-      var event = data.task;
+      var event = data.event;
       // 遍历所有monaco事件
       for(var i = 0; i < event.changes.length; i++) {
         var change = event.changes[i];
